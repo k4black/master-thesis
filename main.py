813a7a677e1ab810c1e2a8470339fe6cc87d7aa2
Konstantin Chernyshev <k4black@ya.ru>
2024-02-12 08:50:00 +0100

    feat: apply style

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import time
 from collections import defaultdict
-from collections.abc import Generator, Callable
+from collections.abc import Generator
 
 import click
 import torch
@@ -22,12 +22,13 @@ from tqdm import tqdm
 from torchinfo import summary
 from torch.utils.hooks import RemovableHandle
 
-
 IS_TPU_AVAILABLE = torch.cuda.is_available() and "TPU" in torch.cuda.get_device_name(0)
 IS_CUDA_AVAILABLE = torch.cuda.is_available() and not IS_TPU_AVAILABLE
 IS_FP16_AVAILABLE = IS_CUDA_AVAILABLE
 # IS_BF16_AVAILABLE = IS_CUDA_AVAILABLE and torch.backends.cuda.is_bf16_supported
-print(f"all devices: {[torch.cuda.get_device_name(i) for i in range(torch.cuda.device_count())] if torch.cuda.is_available() else []}")
+print(
+    f"all devices: {[torch.cuda.get_device_name(i) for i in range(torch.cuda.device_count())] if torch.cuda.is_available() else []}"
+)
 print(f"CUDA_AVAILABLE: {IS_CUDA_AVAILABLE}")
 print(f"FP16_AVAILABLE: {IS_FP16_AVAILABLE}")
 # print(f"BF16_AVAILABLE: {IS_BF16_AVAILABLE}")
@@ -67,11 +68,7 @@ def load_glue_dataset(task_name: str, tokenizer: Tokenizer) -> DatasetDict:
         _tokenize,
         batched=True,
         load_from_cache_file=True,
-        remove_columns=[
-            col
-            for col in dataset["train"].column_names
-            if col not in ["label", "labels"]
-        ],
+        remove_columns=[col for col in dataset["train"].column_names if col not in ["label", "labels"]],
     )
 
     if task_name == "mnli":
@@ -106,9 +103,7 @@ def measure_metric(
     # evaluate
     model.eval()
     start_time = time.perf_counter()
-    for batch in tqdm(
-        dataloader, total=len(dataloader), desc=f"Evaluating {task_name}"
-    ):
+    for batch in tqdm(dataloader, total=len(dataloader), desc=f"Evaluating {task_name}"):
         for k, v in batch.items():
             batch[k] = v.to(model.device, non_blocking=True)
 
@@ -250,15 +245,9 @@ def collect_mask_grads(
 
     # apply masks to model
     handles: list[RemovableHandle] = []
-    handles.extend(
-        apply_ff_neuron_mask(model, ff_neuron_mask)
-    )
-    handles.extend(
-        apply_ff_layer_mask(model, ff_layer_mask)
-    )
-    handles.extend(
-        apply_attention_layer_mask(model, attention_layer_mask)
-    )
+    handles.extend(apply_ff_neuron_mask(model, ff_neuron_mask))
+    handles.extend(apply_ff_layer_mask(model, ff_layer_mask))
+    handles.extend(apply_attention_layer_mask(model, attention_layer_mask))
     # handles.extend(
     #     apply_hidden_state_mask(model, hidden_state_mask)
     # )
@@ -322,10 +311,7 @@ def collect_activations(
         attentions = outputs.attentions  # list of[(batch_size, num_heads, seq_len, seq_len)]
         # average over sequence length (last 2 dimensions)
         head_activations.append(
-            torch.stack([
-                layer.mean(dim=-1).mean(dim=-1)  # (batch_size, num_heads)
-                for layer in attentions
-            ], dim=1)
+            torch.stack([layer.mean(dim=-1).mean(dim=-1) for layer in attentions], dim=1)  # (batch_size, num_heads)
         )
 
     head_activations = torch.cat(head_activations, dim=0)  # (num_samples, num_layers, num_heads)
@@ -363,13 +349,10 @@ def main(
     prune_hidden_state: bool,
     seed: int,
 ) -> None:
-
     # Load the finetuned model and the corresponding tokenizer
     print(f"Loading model {pretrained_model_name_or_path}...")
     config = AutoConfig.from_pretrained(pretrained_model_name_or_path)
-    model = AutoModelForSequenceClassification.from_pretrained(
-        pretrained_model_name_or_path, config=config
-    )
+    model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_name_or_path, config=config)
     if IS_CUDA_AVAILABLE:
         model = model.to("cuda", non_blocking=True)
     for param in model.parameters():
@@ -392,7 +375,11 @@ def main(
     print(f"Sample dataloader: {len(sample_dataloader.dataset)} samples")
 
     validate_dataloader = DataLoader(
-        dataset["validation"].select(range(num_valid_samples)) if len(dataset["validation"]) > num_valid_samples else dataset["validation"],
+        (
+            dataset["validation"].select(range(num_valid_samples))
+            if len(dataset["validation"]) > num_valid_samples
+            else dataset["validation"]
+        ),
         batch_size=batch_size,
         shuffle=False,
         collate_fn=collate_fn,
@@ -405,7 +392,7 @@ def main(
     print(model)
     print(model.config)
 
-    print('-' * 80)
+    print("-" * 80)
 
     # measure before metric
     if IS_FP16_AVAILABLE:
@@ -427,38 +414,42 @@ def main(
         # collect grads
         if IS_FP16_AVAILABLE:
             with torch.cuda.amp.autocast():
-                head_grads, ff_neuron_grads, ff_layer_grads, attention_layer_grads, hidden_state_grads = collect_mask_grads(
+                attention_head_grads, ff_neuron_grads, ff_layer_grads, attention_layer_grads, hidden_state_grads = (
+                    collect_mask_grads(
+                        model,
+                        sample_dataloader,
+                    )
+                )
+        else:
+            attention_head_grads, ff_neuron_grads, ff_layer_grads, attention_layer_grads, hidden_state_grads = (
+                collect_mask_grads(
                     model,
                     sample_dataloader,
                 )
-        else:
-            head_grads, ff_neuron_grads, ff_layer_grads, attention_layer_grads, hidden_state_grads = collect_mask_grads(
-                model,
-                sample_dataloader,
             )
 
         if how_to_average == "fisher_info":
-            head_importance = head_grads.pow(2).sum(dim=0)
+            attention_head_importance = attention_head_grads.pow(2).sum(dim=0)
             ff_neuron_importance = ff_neuron_grads.pow(2).sum(dim=0)
             ff_layer_importance = ff_layer_grads.pow(2).sum(dim=0)
             attention_layer_importance = attention_layer_grads.pow(2).sum(dim=0)
             # hidden_state_importance = hidden_state_grads.pow(2).sum(dim=0)
         elif how_to_average == "mean":
-            head_importance = head_grads.abs().mean(dim=0)
+            attention_head_importance = attention_head_grads.abs().mean(dim=0)
             ff_neuron_importance = ff_neuron_grads.abs().mean(dim=0)
             ff_layer_importance = ff_layer_grads.abs().mean(dim=0)
             attention_layer_importance = attention_layer_grads.abs().mean(dim=0)
             # hidden_state_importance = hidden_state_grads.abs().mean(dim=0)
         elif how_to_average == "entropy":
             # more entropy means more important - less predictable
-            head_importance = - (head_grads * torch.log(head_grads)).sum(dim=0)
-            ff_neuron_importance = - (ff_neuron_grads * torch.log(ff_neuron_grads)).sum(dim=0)
-            ff_layer_importance = - (ff_layer_grads * torch.log(ff_layer_grads)).sum(dim=0)
-            attention_layer_importance = - (attention_layer_grads * torch.log(attention_layer_grads)).sum(dim=0)
+            attention_head_importance = -(attention_head_grads * torch.log(attention_head_grads)).sum(dim=0)
+            ff_neuron_importance = -(ff_neuron_grads * torch.log(ff_neuron_grads)).sum(dim=0)
+            ff_layer_importance = -(ff_layer_grads * torch.log(ff_layer_grads)).sum(dim=0)
+            attention_layer_importance = -(attention_layer_grads * torch.log(attention_layer_grads)).sum(dim=0)
             # hidden_state_importance = - (hidden_state_grads * torch.log(hidden_state_grads)).sum(dim=0)
         else:
             assert False, f"Unknown how_to_average: {how_to_average}"
-        print('head_importance', head_importance.shape)
+        print("attention_head_importance", attention_head_importance.shape)
 
     elif how_to_collect == "activations":
         # collect activations
@@ -475,23 +466,23 @@ def main(
             )
 
         if how_to_average == "fisher_info":
-            head_importance = head_activations.pow(2).sum(dim=0)
+            attention_head_importance = head_activations.pow(2).sum(dim=0)
         elif how_to_average == "mean":
-            head_importance = head_activations.abs().mean(dim=0)
+            attention_head_importance = head_activations.abs().mean(dim=0)
         elif how_to_average == "entropy":
             # more entropy means more important - less predictable
-            head_importance = - (head_activations * torch.log(head_activations)).sum(dim=0)
+            attention_head_importance = -(head_activations * torch.log(head_activations)).sum(dim=0)
         else:
             assert False, f"Unknown how_to_average: {how_to_average}"
-        print('head_importance', head_importance.shape)
+        print("attention_head_importance", attention_head_importance.shape)
 
     elif how_to_collect == "random":
-        head_importance = torch.rand(config.num_hidden_layers, config.num_attention_heads)
+        attention_head_importance = torch.rand(config.num_hidden_layers, config.num_attention_heads)
         ff_neuron_importance = torch.rand(config.num_hidden_layers, config.intermediate_size)
         ff_layer_importance = torch.rand(config.num_hidden_layers)
         attention_layer_importance = torch.rand(config.num_hidden_layers)
         hidden_state_importance = torch.rand(config.hidden_size)
-        print('head_importance', head_importance.shape)
+        print("attention_head_importance", attention_head_importance.shape)
 
     else:
         assert False, f"Unknown how_to_collect: {how_to_collect}"
@@ -500,7 +491,7 @@ def main(
     print(f"Average importance for {how_to_collect}/{how_to_average}:")
     for layer in range(config.num_hidden_layers):
         print(f"> Layer {layer}:")
-        print(f"  Attention Head: {head_importance[layer].mean().item()}")
+        print(f"  Attention Head: {attention_head_importance[layer].mean().item()}")
         print(f"  FF Neuron: {ff_neuron_importance[layer].mean().item()}")
         print(f"  FF Layer: {ff_layer_importance[layer].mean().item()}")
         print(f"  Attention Layer: {attention_layer_importance[layer].mean().item()}")
@@ -508,13 +499,13 @@ def main(
 
     # prune
     for remain_head_percentage in [0.9, 0.7, 0.5, 0.3, 0.1]:
-        print('-'*80)
-        print(f"Pruning {remain_head_percentage*100}% remain on {how_to_collect}/{how_to_average}...")
+        print("-" * 80)
+        print(f"Pruning {remain_head_percentage * 100}% remain on {how_to_collect}/{how_to_average}...")
 
         # load fresh model
         config = AutoConfig.from_pretrained(pretrained_model_name_or_path)
         model = AutoModelForSequenceClassification.from_pretrained(
-            pretrained_model_name_or_path, config=config, ignore_mismatched_sizes=True  #  TODO: check why this is needed
+            pretrained_model_name_or_path, config=config, ignore_mismatched_sizes=True
         )
         for param in model.parameters():
             param.requires_grad_(False)
@@ -522,11 +513,11 @@ def main(
             model = model.to("cuda", non_blocking=True)
 
         if prune_heads:
-            # head_importance  # [num_hidden_layers, num_attention_heads]
+            # attention_head_importance  # [num_hidden_layers, num_attention_heads]
             # collect heads_to_prune: dict of {layer_num: list of heads to prune in this layer}
             # note: first select top k% heads to prune regardless of layer
-            heads_importance_flat = head_importance.view(-1)
-            num_heads_to_prune = int(head_importance.numel() * (1 - remain_head_percentage))
+            heads_importance_flat = attention_head_importance.view(-1)
+            num_heads_to_prune = int(attention_head_importance.numel() * (1 - remain_head_percentage))
             _, heads_to_prune_flat = heads_importance_flat.topk(num_heads_to_prune, largest=False)
             heads_to_prune = defaultdict(list)
             for head_idx in heads_to_prune_flat:
@@ -544,11 +535,18 @@ def main(
             # print layers and number of heads deleted
             total_num_heads_deleted = 0
             for layer in range(config.num_hidden_layers):
-                num_heads_deleted = config.num_attention_heads - model.bert.encoder.layer[layer].attention.self.num_attention_heads
+                num_heads_deleted = (
+                    config.num_attention_heads - model.bert.encoder.layer[layer].attention.self.num_attention_heads
+                )
                 total_num_heads_deleted += num_heads_deleted
                 print(f"> Layer {layer}: {num_heads_deleted} heads deleted")
-            total_percent_heads_deleted = total_num_heads_deleted / (config.num_attention_heads * config.num_hidden_layers)
-            print(f"Total: {total_percent_heads_deleted*100:.2f}% heads deleted, {(1-total_percent_heads_deleted)*100:.2f}% remain")
+            total_percent_heads_deleted = total_num_heads_deleted / (
+                config.num_attention_heads * config.num_hidden_layers
+            )
+            print(
+                f"Total: {total_percent_heads_deleted * 100:.2f}% heads deleted, "
+                f"{(1 - total_percent_heads_deleted) * 100:.2f}% remain"
+            )
 
         if prune_ff_neurons:
             # prune FF neurons
@@ -557,18 +555,29 @@ def main(
             for layer in range(config.num_hidden_layers):
                 ff_intermediate = model.bert.encoder.layer[layer].intermediate.dense
                 ff_output = model.bert.encoder.layer[layer].output.dense
-                neurons_to_prune = ff_neuron_importance[layer].argsort()[:int(config.intermediate_size * remain_head_percentage)]
-                model.bert.encoder.layer[layer].intermediate.dense = prune_linear_layer(ff_intermediate, neurons_to_prune, dim=0)
+                neurons_to_prune = ff_neuron_importance[layer].argsort()[
+                    : int(config.intermediate_size * remain_head_percentage)
+                ]
+                model.bert.encoder.layer[layer].intermediate.dense = prune_linear_layer(
+                    ff_intermediate, neurons_to_prune, dim=0
+                )
                 model.bert.encoder.layer[layer].output.dense = prune_linear_layer(ff_output, neurons_to_prune, dim=1)
 
             # print layers and number of neurons deleted in FF
             total_num_neurons_deleted = 0
             for layer in range(config.num_hidden_layers):
-                num_neurons_deleted = config.intermediate_size - model.bert.encoder.layer[layer].intermediate.dense.out_features
+                num_neurons_deleted = (
+                    config.intermediate_size - model.bert.encoder.layer[layer].intermediate.dense.out_features
+                )
                 total_num_neurons_deleted += num_neurons_deleted
                 print(f"> Layer {layer}: {num_neurons_deleted} neurons deleted")
-            total_percent_neurons_deleted = total_num_neurons_deleted / (config.intermediate_size * config.num_hidden_layers)
-            print(f"Total: {total_percent_neurons_deleted*100:.2f}% neurons deleted, {(1-total_percent_neurons_deleted)*100:.2f}% remain")
+            total_percent_neurons_deleted = total_num_neurons_deleted / (
+                config.intermediate_size * config.num_hidden_layers
+            )
+            print(
+                f"Total: {total_percent_neurons_deleted * 100:.2f}% neurons deleted, "
+                f"{(1 - total_percent_neurons_deleted) * 100:.2f}% remain"
+            )
 
         # measure before metric
         if IS_FP16_AVAILABLE:
@@ -584,7 +593,10 @@ def main(
                 validate_dataloader,
                 task_name,
             )
-        print(f"AFTER {remain_head_percentage*100}% remain on {how_to_collect}/{how_to_average}: {task_name} metric: {metric:.4f} {elapsed_time:.2f}s ({len(validate_dataloader.dataset)} samples)")
+        print(
+            f"AFTER {remain_head_percentage * 100}% remain on {how_to_collect}/{how_to_average}: "
+            f"{task_name} metric: {metric:.4f} {elapsed_time:.2f}s ({len(validate_dataloader.dataset)} samples)"
+        )
 
 
 if __name__ == "__main__":
